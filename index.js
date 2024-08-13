20743c56b1c3ef5d60b4930bd6f4a9d69c4441ea
gnad1209 <danga120902@gmail.com>
2024-08-13 15:36:32 +0700

    a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const express = require('express');
+const express = require("express");
+const routes = require('./route');
 const app = express()
+const port = 9000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+routes(app)
+
+app.listen(port, () => {
+    console.log(`server is running with port: ${port}`)
+})
