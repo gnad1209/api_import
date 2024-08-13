c1dfb8eeffcef424a77d1e8410786bd7e80b9ea5
phuchieu <phuchieu@lifetek.vn>
2024-08-13 16:25:23 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require("express");
+const express = require('express');
 const routes = require('./src/routes/index');
-const app = express()
-const port = 9000
+const { connectToDatabase } = require('./config/db');
+const app = express();
+const port = 9000;
+
+connectToDatabase();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-routes(app)
+routes(app);
 
 app.listen(port, () => {
-    console.log(`server is running with port: ${port}`)
-})
+  console.log(`server is running with port: ${port}`);
+});
