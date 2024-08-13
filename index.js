886016b7eb2c47a6cbfab14aef0f97ef4c0b4831
phuchieu <phuchieu@lifetek.vn>
2024-08-13 16:52:53 +0700

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = require('./src/routes/index');
+const {connectToDatabase} = require('./src/config/db');
+
+connectToDatabase();
+const app = express()
+const port = process.env.PORT || 9000;
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true  }));
+
+routes(app);
+
+app.listen(port, () => {
+    console.log(`server is running with port: ${port}`)
+})
