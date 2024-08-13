a4551854efd50b123875319ed86e96b7e7c8c486
phuchieu <phuchieu@lifetek.vn>
2024-08-13 16:51:59 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
-// const incommingDocumentRoute = require('../modules/incommingDocument/incommingDocument.route');
+const incommingDocumentRoute = require('../modules/incommingDocument/incommingDocument.route');
 const outgoingDocumentRoute = require('../modules/outgoingDocument/outgoingDocument.route');
 
 const routes = (app) => {
-    // app.use('/incommingDocument/', incommingDocumentRoute)
+    app.use('/incommingDocument/', incommingDocumentRoute)
     app.use('/outgoingDocument/', outgoingDocumentRoute)
 }
 module.exports = routes
