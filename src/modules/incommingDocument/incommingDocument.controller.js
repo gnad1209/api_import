d6855a67f73891223695015a44e2f2925e5ebf58
gnad1209 <danga120902@gmail.com>
2024-08-16 09:44:57 +0700

    a

--- src/modules/incommingDocument/incommingDocument.controller.js
+++ src/modules/incommingDocument/incommingDocument.controller.js
@@ -5,22 +5,23 @@ const readAndMapFileFromExcelV3 = async (req, res, next) => {
   try {
     console.log('Xu ly import file');
     const service = require('./incommingDocument.service');
-    let { importFile } = req.files;
-    if (importFile.length < 1) {
+    let { importFile, zipFile } = req.files;
+    if (importFile.length < 1 || zipFile.length < 1) {
       return res.status(400).json({
         status: 0,
         message: 'Upload files failed',
       });
     }
     const importFile0 = importFile[0];
-    const totalSize = importFile0.size;
+    const zipFile0 = zipFile[0];
+    const totalSize = importFile0.size + zipFile0.size;
 
     const processDataConfig = {};
     const { planId, uploaderId, profileId, organizationUnit, clientId } = req.query;
-    if (planId) processDataConfig.plan = planId;
-    if (uploaderId) processDataConfig.creator = uploaderId;
-    if (profileId) processDataConfig.profileId = profileId;
-    if (organizationUnit) processDataConfig.organizationUnit = organizationUnit;
+    // if (planId) processDataConfig.plan = planId;
+    // if (uploaderId) processDataConfig.creator = uploaderId;
+    // if (profileId) processDataConfig.profileId = profileId;
+    // if (organizationUnit) processDataConfig.organizationUnit = organizationUnit;
     if (clientId) processDataConfig.clientId = clientId;
 
     if (clientId) {
@@ -41,7 +42,7 @@ const readAndMapFileFromExcelV3 = async (req, res, next) => {
       }
     }
 
-    const [uploadedImportFile] = await File.create([
+    const [uploadedImportFile, uploadedZipFile] = await File.create([
       {
         name: importFile0.originalname,
         filename: importFile0.filename,
@@ -51,13 +52,27 @@ const readAndMapFileFromExcelV3 = async (req, res, next) => {
         field: importFile0.fieldname,
         user: importFile0.user,
       },
+      {
+        name: zipFile0.originalname,
+        filename: zipFile0.filename,
+        path: zipFile0.path,
+        size: zipFile0.size,
+        mimetype: zipFile0.mimetype,
+        field: zipFile0.fieldname,
+        user: zipFile0.user,
+      },
     ]);
 
     console.log(`Uploaded file ${uploadedImportFile.filename}: `, uploadedImportFile.path);
+    console.log(`Uploaded file ${uploadedZipFile.filename}: `, uploadedZipFile.path);
 
-    const folderSaveFiles = await service.createFolderAndSaveFilesV2(uploadedImportFile.toObject());
+    const folderSaveFiles = await service.createFolderAndSaveFilesV2(
+      uploadedImportFile.toObject(),
+      uploadedZipFile.toObject(),
+    );
 
-    const excelData = await service.getDataFromExcelFile(uploadedImportFile.path);
+    const excelData = await service.getDataFromExcelFile(uploadedImportFile);
+    console.log(uploadedImportFile);
 
     const data = await service.dataProcessing(excelData, folderSaveFiles, processDataConfig);
     // console.log('=================== DONE ===================');
