b31e64ff58e2eccb9ecaaa1edea26eccd4802a9c
phuchieu <phuchieu@lifetek.vn>
2024-08-30 14:26:45 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./src/routes/index');
 const morgan = require('morgan');
+const { connectToDatabase } = require('./src/config/db');
 
 const app = express();
 const port = process.env.PORT || 9000;
 
+connectToDatabase();
 app.use(morgan('dev'));
 
 app.use(express.json({ limit: '50mb' }));
